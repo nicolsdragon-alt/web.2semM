d31107b7835356504a921ef9c0639e3c45ef8db3
nicolsdragon-alt <nicolsdragon@gmail.com>
2026-09-17 11:18:14 -0300

    calculadora

--- main.js
+++ main.js
@@ -1,31 +1,11 @@
-/*Tipos de variáveis
-var -> escopo global
-let -> escopo local
-const -> escopo flex, valor será o mesmo até o fim do programa
-*/
+const formulario = document.getElementById("calcform")
 
-/*Criando variável*/
+formulario.addEventListener("submit", function (e){
+    e.preventDefault();
 
-    /*
-espremedor (fruta){
-    suco
-}
-*/
-/*funcao*/
-function eTriangulo(l1,l2,l3) {
-    return (l1 > 0 && l2 > 0 && l3 > 0) &&
-           ( l1 + l2 > l3)&&
-           (l1 + l3 > l2)&&
-           (l2 + l3 > l1);
-}
-var ladoum = Number(prompt("Qual o valor do lado 1?"));
-var ladodois = Number(prompt("Qual o valor do lado 2?"));
-var ladotres = Number(prompt("Qual o valor do lado 3?"));
+    const a = Number(document.getElementById("num1").value)
+    const b = Number(document.getElementById("num2").value)
 
-var tri = eTriangulo(ladoum,ladodois,ladotres);
-if(tri) {
-    alert("os valores formam um triangulo valido")
-}else{
-    alert("os valores Não formam um triangulo valido")
-}
-
+    const soma = a + b
+    document.getElementById("resultado").textContent = soma
+})
